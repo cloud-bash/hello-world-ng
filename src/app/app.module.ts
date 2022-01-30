199f53b4518a3da92da738702f1214cda687a6ea
Michael Sacks <sysad.sacks@gmail.com>
2022-01-29 19:37:29 -0500

    new services and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
+import { CoursesService } from './courses.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CoursesComponent } from './courses.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CoursesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
